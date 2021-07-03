f0cb765d9258cac24f921a2f460f5579df6c0d44
Nham Phat <phattannham@gmail.com>
2021-07-03 23:31:21 +0700

    [BE] Feat: Get recommended books API - Add relationships for model Book - Add scope selectAvgStar - Add scope selectDiscountPrice

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,68 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Book extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function discounts()
+    {
+        return $this->hasMany(Discount::class)->latest('discount_start_date');
+    }
+
+    public function availableDiscounts()
+    {
+        return $this->hasMany(Discount::class)->latest('discount_start_date')
+            ->whereDate('discount_start_date', '<', now())
+            ->where(function ($query) {
+                $query->whereDate('discount_end_date', '>', now())
+                      ->orWhereNull('discount_end_date');
+            });
+    }
+
+    public function getFinalPriceAttribute()
+    {
+        return $this->discount_price ?? $this->availableDiscounts->first()->discount_price ?? $this->book_price;
+    }
+
+    public function scopeSelectAvgStar($query)
+    {
+        return $query->addSelect([
+            'avg_star' => Review::select(DB::raw('sum(cast(rating_start as integer)) / count(*)'))
+                ->whereColumn('book_id', 'books.id')
+        ]);
+    }
+
+    public function scopeSelectDiscountPrice($query)
+    {
+        return $query->addSelect([
+            'discount_price' => Discount::select('discount_price')
+                ->whereColumn('book_id', 'books.id')
+                ->whereDate('discount_start_date', '<', now())
+                ->where(function ($query) {
+                    $query->whereDate('discount_end_date', '>', now())
+                        ->orWhereNull('discount_end_date');
+                })->limit(1)
+        ]);
+    }
 }
+
