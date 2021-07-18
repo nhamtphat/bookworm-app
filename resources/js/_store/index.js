5b5f304ebe6cbb3a27d3868744b2862b24cf914b
Nham Phat <phattannham@gmail.com>
2021-07-18 23:07:47 +0700

    [FE] Fix: Cart quantity, Implemented redux-persist

--- resources/js/_store/index.js
+++ resources/js/_store/index.js
@@ -1,6 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import rootReducer from 'reducers/index'
+import rootReducer from './reducers/index'
+import {persistReducer, persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-const store =  createStore(rootReducer,applyMiddleware(thunkMiddleware));
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(pReducer, applyMiddleware(thunkMiddleware));
+export const persistor = persistStore(store);
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router'
-import {createStore} from "redux";
-import rootReducer from './_store/reducers';
 import {Provider} from "react-redux";
-
-const store = createStore(rootReducer);
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from "./_store";
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router />
+        <PersistGate loading={null} persistor={persistor}>
+            <Router />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
