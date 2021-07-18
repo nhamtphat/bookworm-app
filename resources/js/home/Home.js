9f254a7189bb2b78149025ff417afabb3448980c
Nham Phat <phattannham@gmail.com>
2021-07-18 21:33:43 +0700

    style: changed homepage template

--- resources/js/home/Home.js
+++ resources/js/home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useEffect, useState} from 'react'
 import Layout from "../layouts";
 import BookGridFigure from "../common/BookGridFigure";
 import Slider from "react-slick";
@@ -6,81 +6,90 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from "axios";
 import {Helmet} from "react-helmet";
+import "./Home.css"
+import {Link} from "react-router-dom";
 
-export default class Home extends Component {
-    state = {
-        onsale_books: [],
-        recommended_books: [],
-        popular_books: [],
-    }
+export default function Home() {
+    const [onSaleBook, setOnSaleBooks] = useState([]);
+    const [recommendedBooks, setRecommendedBooks] = useState([]);
+    const [popularBooks, setPopularBooks] = useState([]);
+    const [view, setView] = useState("recommended");
 
-    componentDidMount() {
+    useEffect(() => {
         axios.get("/api/homepage")
             .then(response => {
-                this.setState({
-                    onsale_books: response.data.onsale_books,
-                    recommended_books: response.data.recommended_books,
-                    popular_books: response.data.popular_books
-                })
+                setOnSaleBooks(response.data.onsale_books)
+                setRecommendedBooks(response.data.recommended_books)
+                setPopularBooks(response.data.popular_books)
             });
-    }
+    }, [])
 
-    render() {
-        const settings = {
-            infinite: true,
-            swipeToSlide: true,
-            slidesToShow: 4,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 640,
-                    settings: {
-                        slidesToShow: 1
-                    }
+    const settings = {
+        infinite: true,
+        swipeToSlide: true,
+        slidesToShow: 4,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1
                 }
-            ]
-        };
+            }
+        ]
+    };
 
-        return (
-            <Layout>
-                <Helmet>
-                    <title>Bookworm Homepage</title>
-                </Helmet>
-                <section className="section-content bg padding-y">
-                    <div className="container">
-                        <h4>Slick slider items</h4>
-                        <Slider {...settings}>
-                        {this.state.onsale_books.map(book => (
-                            <div key={book.id} className="item-slide p-2">
-                                <BookGridFigure book={book} />
-                            </div>
-                        ))}
-                        </Slider>
-
-                        <h4 className="mt-5">Recommended Books</h4>
-                        <div className="row">
-                            {this.state.recommended_books.map(book => (
-                                <div key={book.id} className="col-md-3">
-                                    <div className="item-slide p-2">
-                                        <BookGridFigure book={book} />
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-
-                        <h4 className="mt-5">Popular books</h4>
-                        <div className="row">
-                            {this.state.popular_books.map(book => (
-                                <div key={book.id} className="col-md-3">
-                                    <div className="item-slide p-2">
-                                        <BookGridFigure book={book} />
-                                    </div>
-                                </div>
-                            ))}
+    return (
+        <Layout>
+            <Helmet>
+                <title>Bookworm Homepage</title>
+            </Helmet>
+            <section className="section-content bg padding-y">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4 className="d-inline">Slick slider items</h4>
+                            <Link to="/shop" className="btn btn-secondary float-right">View all <i className="fas fa-caret-right ml-2"></i></Link>
                         </div>
                     </div>
-                </section>
-            </Layout>
-        )
-    }
+                    <Slider {...settings}>
+                        {onSaleBook.map(book => (
+                            <div key={book.id} className="item-slide p-2">
+                                <BookGridFigure book={book}/>
+                            </div>
+                        ))}
+                    </Slider>
+
+                    <div className="p-3 mt-5 text-center">
+                        <h4>Featured Books</h4>
+
+                        <div className="btn-group text-center">
+                            <button
+                                className={`btn btn-outline-secondary ${(view === "recommended")? 'active' : ""}`}
+                                onClick={() => setView("recommended")}
+                                title="List view">
+                                Recommended
+                            </button>
+                            <button
+                                className={`btn btn-outline-secondary ${(view === "popular")? 'active' : ""}`}
+                                onClick={() => setView("popular")}
+                                title="Grid view">
+                                Popular
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        {(view=="recommended" ? recommendedBooks : popularBooks).map(book => (
+                            <div key={book.id} className="col-md-3">
+                                <div className="item-slide p-2">
+                                    <BookGridFigure book={book}/>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    )
 }
