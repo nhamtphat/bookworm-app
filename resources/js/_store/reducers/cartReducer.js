bf7d2c348d6ba4c5e6776e79fa53249b8d153c02
Nham Phat <phattannham@gmail.com>
2021-07-21 00:14:35 +0700

    [FE] Fix: numberCart counter

--- resources/js/_store/reducers/cartReducer.js
+++ resources/js/_store/reducers/cartReducer.js
@@ -18,6 +18,7 @@ export default function cartReducer(state = initProduct, action) {
         case ADD_CART:
             p_product = action.payload.product;
             p_quantity = action.payload.quantity;
+            let plus_number_cart = 0;
 
             if (state.numberCart === 0) {
                 let cart = {
@@ -25,6 +26,7 @@ export default function cartReducer(state = initProduct, action) {
                     quantity: p_quantity,
                 }
                 state.Carts.push(cart);
+                plus_number_cart = 1
             } else {
                 let check = false;
                 state.Carts.map((item, key) => {
@@ -42,16 +44,25 @@ export default function cartReducer(state = initProduct, action) {
                         quantity: p_quantity
                     }
                     state.Carts.push(_cart);
+                    plus_number_cart = 1
                 }
+            }
+
+            if (p_quantity > 0) {
+                toast.success("Added " + p_quantity + " items to cart!");
+            } else {
+                toast.warning("You can only add up to 8 items!");
             }
             return {
                 ...state,
-                numberCart: state.numberCart + p_quantity
+                numberCart: state.numberCart + plus_number_cart
             }
 
         case INCREASE_QUANTITY:
             p_product = action.payload
-            p_quantity = state.Carts.find((item) => { return p_product.id == item.product.id }).quantity;
+            p_quantity = state.Carts.find((item) => {
+                return p_product.id == item.product.id
+            }).quantity;
             if (p_quantity == MAX_QUANTITY) {
                 return {
                     ...state
@@ -59,8 +70,7 @@ export default function cartReducer(state = initProduct, action) {
             }
             return {
                 ...state,
-                numberCart: state.numberCart + 1,
-                Carts:  state.Carts.map((item) => {
+                Carts: state.Carts.map((item) => {
                     if (item.product.id === p_product.id) {
                         return {
                             ...item,
@@ -74,8 +84,10 @@ export default function cartReducer(state = initProduct, action) {
 
         case DECREASE_QUANTITY:
             p_product = action.payload
-            p_quantity = state.Carts.find((item) => { return p_product.id == item.product.id }).quantity;
-            if(p_quantity == 1) {
+            p_quantity = state.Carts.find((item) => {
+                return p_product.id == item.product.id
+            }).quantity;
+            if (p_quantity == 1) {
                 return {
                     ...state,
                     numberCart: state.numberCart - 1,
@@ -87,8 +99,7 @@ export default function cartReducer(state = initProduct, action) {
 
             return {
                 ...state,
-                numberCart: state.numberCart - 1,
-                Carts:  state.Carts.map((item) => {
+                Carts: state.Carts.map((item) => {
                     if (item.product.id === p_product.id) {
                         return {
                             ...item,
