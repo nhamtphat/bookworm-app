329fb464ed52aaea3defb539d3afd7fd5a82fb3e
Nham Phat <phattannham@gmail.com>
2021-07-23 14:18:11 +0700

    fix: max display length of pagination

--- resources/js/common/BookListFigure.js
+++ resources/js/common/BookListFigure.js
@@ -1,7 +1,7 @@
-import React  from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function BookListFigure ({book}) {
+export default function BookListFigure({ book }) {
     return (
         <article className="card card-product-list">
             <div className="row no-gutters">
@@ -13,7 +13,10 @@ export default function BookListFigure ({book}) {
                 </aside>
                 <div className="col-md-6">
                     <div className="info-main">
-                        <Link to={'/books/' + book.id} className="h5 title"> {book.book_title} </Link>
+                        <Link to={"/books/" + book.id} className="h5 title">
+                            {" "}
+                            {book.book_title}{" "}
+                        </Link>
                         <div className="text-danger">{book.author_name}</div>
                         <p> {book.book_summary} </p>
                     </div>
@@ -21,15 +24,23 @@ export default function BookListFigure ({book}) {
                 <aside className="col-sm-3">
                     <div className="info-aside">
                         <div className="price-wrap">
-                            <span className="price h5"> {book.final_price} </span>
-                            {(book.final_price != book.book_price) ? (
-                                <del className="price-old">${book.book_price}</del>
+                            <span className="price h5">
+                                {" "}
+                                {book.final_price}{" "}
+                            </span>
+                            {book.final_price != book.book_price ? (
+                                <del className="price-old">
+                                    ${book.book_price}
+                                </del>
                             ) : null}
                         </div>
                         <p className="text-success">Free shipping</p>
                         <br />
                         <p>
-                            <Link to={"/books/" + book.id} className="btn btn-primary btn-block">
+                            <Link
+                                to={"/books/" + book.id}
+                                className="btn btn-primary btn-block"
+                            >
                                 <i className="fa fa-eye"></i>
                                 <span className="text">View product</span>
                             </Link>
--- resources/js/common/PaginationItem.js
+++ resources/js/common/PaginationItem.js
@@ -1,9 +1,18 @@
-export default function PaginationItem (props) {
-    const {active, disabled, page, text, setPage} = props;
+export default function PaginationItem(props) {
+    const { active, disabled, page, text, setPage } = props;
 
     return (
-        <li className={`page-item ${active? "active" : ""} ${disabled? "disabled" : ""}`} style={{ cursor: "pointer" }}>
-            <a className="page-link" onClick={() => setPage(page)}>{text}</a>
+        <li
+            className={
+                "page-item " +
+                (active ? "active " : "") +
+                (disabled ? "disabled " : "")
+            }
+            style={{ cursor: disabled ? "not-allowed " : "pointer" }}
+        >
+            <a className="page-link" onClick={() => setPage(page)}>
+                {text}
+            </a>
         </li>
-    )
+    );
 }
